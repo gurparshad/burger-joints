e6c6f29eb25971f4f63d22a0beccdabf12769e81
Gurparshad Sharma <gurparshad.sharma@visioncraft.ee>
2024-03-18 01:47:06 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'burger-joints';
+export class AppComponent implements OnInit {
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef<HTMLDivElement>;
+
+  map!: google.maps.Map;
+  circle!: google.maps.Circle;
+  center: google.maps.LatLngLiteral = {
+    lat: 58.3776,
+    lng: 26.7291
+  };
+  zoom = 9;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+    this.map = new google.maps.Map(this.mapContainer.nativeElement, {
+      center: this.center,
+      zoom: this.zoom,
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    this.circle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: this.map,
+      center: this.center,
+      radius: 1000
+    });
+  }
+
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) {
+      this.center = event.latLng.toJSON();
+      this.map.setCenter(event.latLng);
+      this.circle.setCenter(event.latLng);
+    }
+  }
+
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) {
+      this.center = event.latLng.toJSON();
+    }
+  }
 }
